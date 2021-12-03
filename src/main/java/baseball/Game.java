91d0691abc712f4f777b7329bb9169981ca0469c
who-hoo <who.ho3ov@gmail.com>
2021-12-04 03:51:51 +0900

    feat: 사용자 입력받기 구현(예외처리 X)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,14 +1,24 @@
 package baseball;
 
+import baseball.view.InputView;
 import java.util.*;
 
 public class Game {
 
     private final Random random = new Random();
     private final List<Integer> answer = new ArrayList<>();
+    private final InputView input = new InputView();
+    private boolean isRunning = true;
 
     public Game() {
         initAnswer();
+    }
+
+    public void run() {
+        while (isRunning) {
+            int userGuess = input.getUserInput();
+            System.out.println(userGuess);
+        }
     }
 
     private void initAnswer() {
---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner sc = new Scanner(System.in);
+
+    public int getUserInput() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        return Integer.parseInt(sc.nextLine());
+    }
+}
