2e53ae41df66b1f5f931f71c77f69338df95e3bb
who-hoo <who.ho3ov@gmail.com>
2021-12-15 17:11:28 +0900

    :white_check_mark: SetTest 요구사항 3 테스트 케이스 추가
    CsvSource를 활용해 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -32,5 +34,15 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2, 3:true", "4, 5:false"}, delimiter = ':')
+    @DisplayName("입력 값에 따라 결과 값이 다른 contains 테스트")
+    void containsWithExpectedValue(String value, boolean expected) {
+        String[] values = value.split(", ");
+        for (String v: values) {
+            assertThat(numbers.contains(Integer.parseInt(v))).isEqualTo(expected);
+        }
     }
 }
