34d4b30e14058c8a21c2afe662f49b363e3edb52
who-hoo <who.ho3ov@gmail.com>
2021-12-15 16:02:06 +0900

    :white_check_mark: StringTest 요구사항 1 테스트 케이스 추가
    split 학습 테스트 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,9 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual1 = "1,2".split(",");
+        assertThat(actual1).contains("1", "2");
+
+        String[] actual2 = "1".split(",");
+        assertThat(actual2).containsExactly("1");
     }
 }
