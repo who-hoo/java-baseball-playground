67afc264bba298a633bc87b0d19cedd598a32df4
who-hoo <who.ho3ov@gmail.com>
2021-12-15 16:52:47 +0900

    :white_check_mark: SetTest 요구사항 2 테스트 케이스 추가
    ParameterizedTest를 활용해 중복 코드를 제거한 contains 학습 테스트 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -24,5 +26,11 @@ public class SetTest {
     void size() {
         int actual = numbers.size();
         assertThat(actual).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
