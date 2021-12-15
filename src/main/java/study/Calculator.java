1b78767aa52d966c9b36c26cf166de4f80bd0c35
who-hoo <who.ho3ov@gmail.com>
2021-12-15 15:38:22 +0900

    :sparkles: 사칙연산이 가능한 계산기 구현
    테스트 맛보기로 사칙연산이 가능한 계산기 프로그램 Calculator 구현, main method로 테스트

---
+++ src/main/java/study/Calculator.java
@@ -0,0 +1,28 @@
+package study;
+
+public class Calculator {
+
+    public int add(int i, int j) {
+        return i + j;
+    }
+
+    public int subtract(int i, int j) {
+        return i - j;
+    }
+
+    public int multiply(int i, int j) {
+        return i * j;
+    }
+
+    public int divide(int i, int j) {
+        return i / j;
+    }
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(3, 4));
+        System.out.println(cal.subtract(5, 4));
+        System.out.println(cal.multiply(2, 6));
+        System.out.println(cal.divide(8, 4));
+    }
+}
