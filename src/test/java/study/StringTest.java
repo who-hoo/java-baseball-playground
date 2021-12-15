1cd818274aa7285649989239eca26902f41f4540
who-hoo <who.ho3ov@gmail.com>
2021-12-15 16:23:13 +0900

    :white_check_mark: StringTest 요구사항 3 테스트 케이스 추가
    charAt 학습 테스트 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -28,5 +29,22 @@ public class StringTest {
             actual.indexOf("(") + 1, actual.lastIndexOf(")")
         );
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 가져오는 charAt")
+    void charAt() {
+        char actual = "abc".charAt(0);
+        assertThat(actual).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나는 charAt")
+    void charAtException() {
+        assertThatThrownBy(() -> {
+            char actual = "abc".charAt(10);
+        })
+            .isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 10");
     }
 }
